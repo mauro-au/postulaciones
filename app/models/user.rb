fb7fe6614b7fef74871f53d8cad67b4322a5f0e4
mauro-au <mauricio.a.developer@gmail.com>
2025-01-14 03:58:21 -0300

    Se agrega navbar con el menú y condición para que se muestre o no el registro
    - Además de que el primer usuario registrado queda como administrador

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create :only_one_admin
+
+  private
+
+  def only_one_admin
+    if User.count == 0
+      self.isadmin = true
+    end
+  end
 end
